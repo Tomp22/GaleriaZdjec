cc3eb58ef7a2cab4ad0fd02d90945b5a89ecf66a
Tomasz <prusak@hotmail.com>
2022-01-25 19:04:57 +0000

    poprawki do login2

--- app/views/index.php
+++ app/views/index.php
@@ -1,6 +1,7 @@
+<header class="welcome">
 <h1> Hello <?php echo $name ?></h1>
-<h2>Welcome to Micron PHP. The simple framework for making simple web apps</h2>
-
+<h2>Witamy w aplikacji do zarzadzania dokumentami</h2>
+</header>
 <?php
 loadView('login',["title" => 'Sample Title']);
 
