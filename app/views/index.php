8e436d340dc45a5d5810f75e3844a3accb754d65
Tomasz <prusak@hotmail.com>
2022-01-25 10:30:40 +0000

    add dashboard link

--- app/views/index.php
+++ app/views/index.php
@@ -1,3 +1,3 @@
 <h1> Hello <?php echo $name ?></h1>
 <h2>Welcome to Micron PHP. The simple framework for making simple web apps</h2>
-<a href="<?php echo route('dashboard',array('a' => 'add'))?>" class="btn btn-primary">Dashboard</a>
+<a href="<?php echo route('dashboard')?>" class="btn btn-primary">Dashboard</a>
